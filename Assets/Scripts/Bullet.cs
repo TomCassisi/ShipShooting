a7ec3b75b021b9228cf4fcd05debaa01c32ee65b
Chris Cameron <chris.cameron@wedoresi.com>
2018-12-18 16:28:05 -0500

    feat: Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	[SerializeField]
+	private Vector3 m_Velocity; 
+
+	public Vector3 Velocity { get { return m_Velocity; } set { m_Velocity = value; } }
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    Vector3 offset = m_Velocity * Time.deltaTime;
+		Vector3 currentPosition = transform.position;
+	    Vector3 nextPosition = offset + currentPosition;
+
+	    transform.position = nextPosition;
+	}
+}
---
+++ Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+	[SerializeField]
+	private float m_Speed;
+
+	[SerializeField]
+	private Bullet m_Bullet;
+
+	public float Speed { get { return m_Speed; } set { m_Speed = value; } }
+
+	// Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// Movement
+	    float horizontal = Input.GetAxis("Horizontal");
+	    float vertical = Input.GetAxis("Vertical");
+
+	    float verticalSpeed = m_Speed * vertical * Time.deltaTime;
+	    float horizontalSpeed = m_Speed * horizontal * Time.deltaTime;
+
+		Vector3 offset = new Vector3(horizontalSpeed, verticalSpeed, 0);
+	    Vector3 currentPosition = transform.position;
+	    Vector3 nextPosition = offset + currentPosition;
+
+	    transform.position = nextPosition;
+
+		// Shooting
+	    if (Input.GetButtonDown("Fire1"))
+	    {
+		    Bullet bullet = Instantiate(m_Bullet, transform.position, Quaternion.identity);
+			bullet.Velocity = new Vector3(1,0,0);	
+	    }
+
+    }
+
+	void OnGUI()
+	{
+		GUILayout.Label(string.Format("Horizontal {0}", Input.GetAxis("Horizontal")));
+		GUILayout.Label(string.Format("Vertical {0}", Input.GetAxis("Vertical")));
+	}
+}
